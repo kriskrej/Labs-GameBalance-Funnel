5862c6ff631e55cbf6b7c6058f7e578254fa11f3
Kris Krej <kris@krej.net>
2020-09-27 14:59:37 +0200

    empty project - table

---
+++ Assets/Code/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game 
+{
+ 
+}
+
+public class Level {
+    public string name;
+    public float difficulty01;
+    public float timeConsuming01;
+    public float fun01;
+    public float polished01;
+}
